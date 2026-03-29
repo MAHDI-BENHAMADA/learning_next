4cc822c026381b223ead4e7b75339f22e2826e74
MAHDI-BENHAMADA <mehdibenhammada07@gmail.com>
2026-03-29 23:25:30 +0100

    working with clients and server components

---
+++ app/users/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import AddToCart from './addToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+     <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ app/users/components/addToCart.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const addToCart = () => {
+  return (
+    <div>
+       <button  onClick={() => console.log("Product added to cart!")}>Add to Cart</button>
+    </div>
+  )
+}
+
+export default addToCart
