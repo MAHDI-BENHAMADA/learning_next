3c9db5bedaae3d486592ca4b37400ccca2cc2a20
MAHDI-BENHAMADA <mehdibenhammada07@gmail.com>
2026-03-29 23:00:48 +0100

    basic overview about routing and navigation in next.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
+import Image from "next/image"; 
+import Link from "next/link"
 export default function Home() {
   return (
-    <main><h1>Welcome to Next.js!</h1></main>
+    <main><h1>Welcome to Next.js!</h1>
+    <Link href="/users">Users</Link>
+    </main>
   );
 }
---
+++ app/users/newUser/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const newUserPage = () => {
+  return (
+    <div>
+      <h1>New User Page</h1>
+    </div>
+  )
+}
+
+export default newUserPage
