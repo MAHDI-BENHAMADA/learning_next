303124368ea6e65aacaa100b5b77e59cfb54502c
MAHDI-BENHAMADA <mehdibenhammada07@gmail.com>
2026-03-30 23:33:28 +0100

    learning about caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,12 +7,12 @@ interface User {
 }
 
 const users = async () => {
-   const res = await fetch('https://jsonplaceholder.typicode.com/users')
+   const res = await fetch('https://jsonplaceholder.typicode.com/users', {next : {revalidate : 10}}) // this mean that every 10 sec it will go and fetch some new data
    const users: User[] = await res.json()
   return (
     <div>
         <h1>Users Page</h1>
-       {users.map(user => (
+        {users.map(user => (
         <li key={user.id}>{user.name} -- {user.email}</li>
 
        ))}
